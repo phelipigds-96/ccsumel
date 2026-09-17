b30b439e89861ecdf2c4c47c4c89b491f5a53a31
phelipigds-96 <phelipi.gds@gmail.com>
2026-09-17 17:12:50 -0300

    Adiciona Toaster global no RootComponent

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,6 +13,7 @@ import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { AuthProvider } from "../lib/auth";
 import logoAsset from "../assets/logo-sumel.png";
+import { Toaster } from "@/components/ui/sonner";
 
 function NotFoundComponent() {
   return (
@@ -134,6 +135,7 @@ function RootComponent() {
       <AuthProvider>
         {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
         <Outlet />
+        <Toaster />
       </AuthProvider>
     </QueryClientProvider>
   );
